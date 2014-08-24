0c4ec0ca2f028e6d27a1e27474a99882107ae2d4
Pupa <hjf004@gmail.com>
2014-08-24 22:25:18 +0800

    adjust indent

--- src/com/scb/Game.java
+++ src/com/scb/Game.java
@@ -3,13 +3,12 @@
 /**
  * 游戏逻辑类
  * @author Group 5
- *
  */
 public class Game {
 
-	private int[][] grid;	//二维数组存储当代的生命
-	private int[][] nextGrid;	//二维数组存储下一代生命，用作缓存
-	
+	private int[][] grid; // 二维数组存储当代的生命
+	private int[][] nextGrid; // 二维数组存储下一代生命，用作缓存
+
 	/**
 	 * 设置grid值
 	 * @param grid
@@ -19,10 +18,9 @@ public class Game {
 		if (grid != null)
 			nextGrid = new int[grid.length][grid[0].length];
 	}
-	
+
 	/**
-	 * @author  杨海峰
-	 * 计算某个cell周围存活体的个数
+	 * @author 杨海峰 计算某个cell周围存活体的个数
 	 * @param r 行数
 	 * @param c 列数
 	 * @return 返回存活体的个数,-1代表出现错误
@@ -38,11 +36,9 @@ public class Game {
 		count -= grid[r][c];
 		return count;
 	}
-    
-    
-    /**
-	 * @author  郭杨杨
-	 * 判断某个live cell是否太寂寞(has fewer than two live neighbors)
+
+	/**
+	 * @author 郭杨杨 判断某个live cell是否太寂寞(has fewer than two live neighbors)
 	 * @param r 行数
 	 * @param c 列数
 	 * @return true live neighbors 少于两个，false live neighbors在两个或两个以上
@@ -52,14 +48,12 @@ public class Game {
 			return true;
 		return false;
 	}
-    
-    
-    /**
-	 * @author  钟明阳
-	 * 判断某个live cell是否太拥挤(has more than three live neighbors)
+
+	/**
+	 * @author 钟明阳 判断某个live cell是否太拥挤(has more than three live neighbors)
 	 * @param r 行数
 	 * @param c 列数
-	 * @return true  live neighbors超过三个，false   live neighbors没有超过三个
+	 * @return true live neighbors超过三个，false live neighbors没有超过三个
 	 */
 	public boolean ifTooCrowd(int r, int c) {
 		if (calculateLifeAround(r, c) > 3) {
@@ -68,15 +62,12 @@ public class Game {
 			return false;
 		}
 	}
-    
-    
-    
-    /**
-	 * @author  钱海龙
-	 * 判断某个dead cell是否能够复活(has exactly three live neighbors)
+
+	/**
+	 * @author 钱海龙 判断某个dead cell是否能够复活(has exactly three live neighbors)
 	 * @param r 行数
 	 * @param c 列数
-	 * @return true  live neighbors恰好三个，false   live neighbors不是三个
+	 * @return true live neighbors恰好三个，false live neighbors不是三个
 	 */
 	public boolean ifCanRevive(int r, int c) {
 		if (3 == calculateLifeAround(r, c))
@@ -84,15 +75,13 @@ public class Game {
 		else
 			return false;
 	}
-    
-    
-    /**
-     * @author 邓袁
-     * 判断某个 live cell是否能够继续活着（has two or three live neighbors)
-     * @param r 行数
-     * @param c 列数
-     * @return true live neighbors = 2 or 3; 否则, return false
-     */
+
+	/**
+	 * @author 邓袁 判断某个 live cell是否能够继续活着（has two or three live neighbors)
+	 * @param r 行数
+	 * @param c 列数
+	 * @return true live neighbors = 2 or 3; 否则, return false
+	 */
 	public boolean ifCanLiving(int r, int c) {
 		if (this.ifTooLonely(r, c) || this.ifTooCrowd(r, c))
 			return false;
@@ -101,11 +90,10 @@ public class Game {
 
 	}
 
-    /**
-     * @author 胡季风
-     * 根据规则产生下一代
-     * @return 下一代二维数组
-     */
+	/**
+	 * @author 胡季风 根据规则产生下一代
+	 * @return 下一代二维数组
+	 */
 	public int[][] nextGeneration() {
 		for (int i = 1; i < grid.length - 1; ++i)
 			for (int j = 1; j < grid[0].length - 1; ++j) {
@@ -124,9 +112,9 @@ public class Game {
 		return grid;
 	}
 
-    /**
-     * 打印指定的Grid
-     */
+	/**
+	 * 打印指定的Grid
+	 */
 	private void printGrid(int[][] grid) {
 		for (int i = 0; i < grid.length; i++) {
 			for (int j = 0; j < grid[0].length; j++) {
@@ -136,10 +124,10 @@ public class Game {
 		}
 		System.out.println();
 	}
-    
-    /**
-     * 打印Game的Grid
-     */
+
+	/**
+	 * 打印Game的Grid
+	 */
 	public void printGrid() {
 		printGrid(grid);
 	}
